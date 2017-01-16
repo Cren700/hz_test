c63117c362395beaca66757062d9608f64351de9
cren <649460214@qq.com>
2017-01-16 21:11:20 +0800

    send

--- pc/application/controllers/Send.php
+++ pc/application/controllers/Send.php
@@ -12,7 +12,7 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-class sendSMS extends CI_Controller
+class Send extends CI_Controller
 {
     public function __construct()
     {
