12e6e9292da7a76a7f779f6c675db09e73dc2c4e
cren <649460214@qq.com>
2017-03-10 21:05:17 +0800

    pay back

--- mobile/application/controllers/Pay.php
+++ mobile/application/controllers/Pay.php
@@ -73,8 +73,8 @@ class Pay extends HZ_Controller
         //通知地址，必填项，接收威富通通知的URL，需给绝对路径，255字符内格式如:http://wap.tenpay.com/tenpay.asp
         //$notify_url = 'http://'.$_SERVER['HTTP_HOST'];
         //$this->reqHandler->setParameter('notify_url',$notify_url.'/payInterface/request.php?method=callback');
-        $this->reqHandler->setParameter('notify_url', getBaseUrl('pay/payBack.html')); // 接收威富通通知的 URL，需给绝对路径 (支付通知)
-        $this->reqHandler->setParameter('callback_url', getBaseUrl('info/planList.html')); // 支付完成回调页面
+        $this->reqHandler->setParameter('notify_url', getBaseUrl('/pay/payBack.html')); // 接收威富通通知的 URL，需给绝对路径 (支付通知)
+        $this->reqHandler->setParameter('callback_url', getBaseUrl('/info/planList.html')); // 支付完成回调页面
         $this->reqHandler->setParameter('nonce_str',mt_rand(time(),time()+rand()));//随机字符串，必填项，不长于 32 位
         $this->reqHandler->createSign();//创建签名
 
@@ -103,10 +103,14 @@ class Pay extends HZ_Controller
     // 支付通知
     public function payBack(){
         $xml = file_get_contents('php://input');
+        file_put_contents("/tmp/TEST_PAY.log", 'payBack/', FILE_APPEND);
         $this->resHandler->setContent($xml);
         $this->resHandler->setKey($this->cfg->C('key'));
         if($this->resHandler->isTenpaySign()){
             if($this->resHandler->getParameter('status') == 0 && $this->resHandler->getParameter('result_code') == 0){
+
+                file_put_contents("/tmp/TEST_PAY.log", 'status:'.$this->resHandler->getParameter('status').'/code:'.$this->resHandler->getParameter('result_code').'/pay_result:'.$this->resHandler->getParameter('pay_result'), FILE_APPEND);
+                file_put_contents("/tmp/TEST_PAY.log", 'content:'.json_encode_data($this->resHandler->getAllParameters()), FILE_APPEND);
 
                 // pay_result Int 支付结果:0—成功;其它—失败
                 if ($this->resHandler->getParameter('pay_result') === 0) {
@@ -150,7 +154,8 @@ class Pay extends HZ_Controller
     public function dopay($token_id)
     {
         $url = "https://pay.swiftpass.cn/pay/jspay?token_id=".$token_id."&showwxtitle=1";
-        redirect($url);
+        header("Location: {$url}", true, 302);
+        exit();
     }
 
 }
